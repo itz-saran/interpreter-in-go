7e037694819ff99aaa503e9d8a14d01c98b32bc2
saran <saran.v@codingmart.com>
2025-03-03 15:39:44 +0530

    evaluator initial code

---
+++ evaluator/evaluator.go
@@ -0,0 +1,10 @@
+package evaluator
+
+import (
+	"example.com/ast"
+	"example.com/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	return nil
+}
---
+++ object/object.go
@@ -0,0 +1,50 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGEROBJ = "INTEGER"
+	BOOLEANOBJ = "BOOLEAN"
+	NULLOBJ    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGEROBJ
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (b *Boolean) Type() string {
+	return BOOLEANOBJ
+}
+
+type Null struct{}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
+
+func (n *Null) Type() string {
+	return NULLOBJ
+}
